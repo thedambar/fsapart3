ccf9e21fd00ce7967c830172453b2e08ebccef24
thedambar <dambar@gmail.com>
2022-10-11 21:12:43 +0300

    Exercise 3.3

--- index.js
+++ index.js
@@ -33,6 +33,16 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
 app.get('/info', (request, response) => {
   const theDate = new Date()
   response.send(`<p>Phonebook has info for ${persons.length} people<p><p>${theDate}</p>`)
