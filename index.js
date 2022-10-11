f7ed7a29a59ebbb305cdd59e437e4a3e85479497
thedambar <dambar@gmail.com>
2022-10-12 00:10:36 +0300

    Exercise 3.8

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express()
 const morgan = require('morgan')
 
 app.use(express.json())
-app.use(morgan('tiny'))
+morgan.token('body', (req, res) => JSON.stringify(req.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms - :body'))
 
 
 let persons = 
